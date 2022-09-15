1229edf257fa7b33146f7366db57fb8dc68a3447
Amirmahdy <amir.malayjerdi@gmail.com>
2022-09-15 18:45:31 +0430

    Add constraint to model

--- src/cicd/models.py
+++ src/cicd/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
 
 class GithubToken(models.Model):
@@ -13,6 +14,14 @@ class Repo(models.Model):
     owner = models.CharField(max_length=128, verbose_name="Owner of repo")
     repository = models.CharField(max_length=128, verbose_name="Repository")
     token = models.ForeignKey(GithubToken, default=None, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["owner", "repository"],
+                name="unique_parameter_for_repository",
+            )
+        ]
 
     def __str__(self) -> str:
         return f"{self.owner}/{self.repository}"
